37dd23a6466c73bf74199aadd771dfd3e021fb05
⚡ŞŦΔŘŁØŘĐ <soumodip.dgp@gmail.com>
2025-03-17 14:25:36 +0530

    Update namespace.c

--- fs/namespace.c
+++ fs/namespace.c
@@ -1654,7 +1654,6 @@ int ksys_umount(char __user *name, int flags)
 
 	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
 		return -EINVAL;
-
 	if (!may_mount())
 		return -EPERM;
 
